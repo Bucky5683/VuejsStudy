b93494184231ed4597fc5f76f144a0ab382e8c4c
Bucky <bucky5683@gmail.com>
2024-04-08 11:14:38 +0900

    리액트보다 100배 쉬운 Vue 반복문 v-for

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,15 +1,6 @@
 <template>
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p>50만원</p>
-  </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>50만원</p>
-  </div>
-  <div>
-    <h4>{{ products[2] }}</h4>
-    <p>50만원</p>
+  <div class="menu">
+    <a v-for="(작명, i) in 메뉴들" :key="i"> {{ 작명 }}</a>
   </div>
 </template>
 
@@ -18,8 +9,7 @@ export default {
   name: 'App',
   data() {
     return {
-      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
-      스타일: 'color:red'
+      메뉴들: ['Home', 'Shop', 'About']
     }
   }
 }
@@ -34,5 +24,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
