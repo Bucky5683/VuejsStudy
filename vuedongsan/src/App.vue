43aabc7fa3408aaad26eed0dad8b32896930696c
Bucky <bucky5683@gmail.com>
2024-04-08 13:01:46 +0900

    v-if 와 모달창 만들기 (Vue에서 동적인 UI 만드는 법) & 숙제

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <button @click="ifClickedClosedButton">닫기</button>
+      <h4>상세페이지</h4>
+      <p>상세페이지내용임</p>
+    </div>
+  </div>
   <div class="menu">
     <a v-for="(작명, i) in 메뉴들" :key="i"> {{ 작명 }}</a>
   </div>
@@ -7,7 +14,7 @@
       <h4>{{ 제목 }}</h4>
       <p>50만원</p>
       <button @click="increase(i)">허위매물신고</button>
-      <span>신고수 : {{신고수[i]}} </span>
+      <span>신고수 : {{ 신고수[i] }} </span>
     </div>
   </div>
 </template>
@@ -17,16 +24,21 @@ export default {
   name: 'App',
   data() {
     return {
-      신고수 : [0,0,0],
+      모달창열렸니: true,
+      신고수: [0, 0, 0],
       메뉴들: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
   },
-  methods : {
-  increase(i){
-    this.신고수[i] += 1
+  methods: {
+    increase(i) {
+      this.신고수[i] += 1
+    },
+
+    ifClickedClosedButton() {
+      this.모달창열렸니 = false
+    }
   }
-}
 }
 
 </script>
@@ -50,5 +62,28 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
