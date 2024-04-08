0a96d446e677291a331a54daa943207fec5285a4
Bucky <bucky5683@gmail.com>
2024-04-08 11:07:34 +0900

    숙제

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
-    <h4 :style="스타일">XX 원룸</h4>
-    <p>XX 만원</p>
+    <h4>{{ products[0] }}</h4>
+    <p>50만원</p>
   </div>
   <div>
-    <h4>XX 원룸</h4>
-    <p>XX 만원</p>
+    <h4>{{ products[1] }}</h4>
+    <p>50만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>50만원</p>
   </div>
 </template>
 
 <script>
 export default {
-  name : 'App',
-  data(){
+  name: 'App',
+  data() {
     return {
-      price1 : 60,
-      스타일 : 'color:red'
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      스타일: 'color:red'
     }
   }
 }
